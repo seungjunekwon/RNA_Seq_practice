fed389b47fe56fcece99eb8880f4637f44d973cf
Seung June Kwon <udnjf251@gmail.com>
2023-06-02 16:19:33 +0900

    Update pca.R

--- pca.R
+++ pca.R
@@ -1,13 +1,16 @@
 Data <- read.csv("cm.csv", header=T)
 CountData <- read.csv("cm.csv", header=T, row.names = 1)
-GeneIDs <- Data[,1]
+GeneIDs <- Data[, 1]
 
 library(edgeR) # Google "edgeR install"
 
-Condition <- c("Ctrl","Ctrl","Ctrl", "Lin", "Lin", "Lin")
+Condition <- c("Ctrl", "Ctrl", "Ctrl", "Lin", "Lin", "Lin")
 SampleID <- colnames(CountData)
 
-targets <- data.frame(SampleID ,Condition)
+MetaData <- data.frame(SampleID, Condition)
+targets <- MetaData
+
+targets <- data.frame(SampleID, Condition)
 targets$Condition <- factor(targets$Condition)
 
 levels(targets$Condition)
