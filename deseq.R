6aba93abd262e01b6295c59fd43803399da0ec54
Seung June Kwon <udnjf251@gmail.com>
2023-06-09 13:24:39 +0900

    Update deseq.R

--- deseq.R
+++ deseq.R
@@ -39,4 +39,20 @@ write.csv(normCounts.df, "NormCounts.csv")
 
 # 基本的なボルケーノプロットを作成（色やサイズなど、詳しい調整の仕方は Google " EnhancedVolcano "）
 EnhancedVolcano(res.df, x = "log2FoldChange", y = "padj", lab = res.df$symbol,
-                pCutoff = 1e-4, FCcutoff = 1)
+                pCutoff = 1e-4, FCcutoff = 1) # 閾値の設定
+
+# 1つの遺伝子のみを見る
+
+EGR1 <- c("EGR1")
+
+EnhancedVolcano(res.df, x = "log2FoldChange", y = "padj", lab = res.df$symbol,
+                pCutoff = 0, FCcutoff = 0, selectLab = EGR1, drawConnectors = TRUE, max.overlaps = Inf)
+
+# 複数の遺伝子をピックアップして見る
+
+pickup <- c("SALL4", "LIN28A", "TRIM71", "IRX3", "FGF10", "HOXD9", "MEIS2", "TBX5", "SALL1",
+            "MSX1", "TSHZ2", "ZBTB16", "PRDM16", "ETV4", "TFAP2A", "MSX2", "LHX9", "DUSP6", "AXIN2",
+            "ACTC1", "GATA4", "FOXF1", "HAND1", "KRT8", "BMP5", "TAGLN")
+
+EnhancedVolcano(res.df, x = "log2FoldChange", y = "padj", lab = res.df$symbol,
+                pCutoff = 0, FCcutoff = 0, selectLab = pickup, drawConnectors = TRUE, max.overlaps = Inf)
